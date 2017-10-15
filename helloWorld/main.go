db89c17c813d241a8e9acc871f277b1e7a129a8f
Laszlo Suslecz <drsuslac@gmail.com>
2017-10-15 18:15:00 +0200

    delete playground notes

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -4,18 +4,6 @@ import (
 	"fmt"
 )
 
-var sum int //= 0
-
 func main() {
 	fmt.Println("Hello world")
-	/*
-		for i := 0; i < 10; i++ {
-			sum++
-			fmt.Println(sum)
-		}
-	*/
-	for sum < 10 {
-		sum++
-		fmt.Println(sum)
-	}
 }
